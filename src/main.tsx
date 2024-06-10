6ab3431c4150cba1c58f66202c839a89267cc72e
Morsi <136155900+Mahmoud-Morsi0@users.noreply.github.com>
2024-06-10 16:38:31 +0300

    fixed issue at store

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { UserProvider } from './context/userContext.tsx'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Provider } from '@radix-ui/react-toast'
-import { Store } from 'store/store.js';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { UserProvider } from './context/userContext.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider } from 'react-redux'; // Correct Provider import for Redux
+import { store } from './store/store.js'; // Ensure correct path
+import { User } from './types/User';
 
-const queryClint = new QueryClient({
+const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: Infinity,
-    }
-  }
-})
-
-
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider >
-    <UserProvider>
-      <QueryClientProvider client={queryClint}>
-        <App />
-      </QueryClientProvider>
-    </UserProvider>
-    </Provider>
-  </React.StrictMode>,
-)
+  <Provider store={store}>
+    <React.StrictMode>
+      <UserProvider>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </UserProvider>
+    </React.StrictMode>
+  </Provider>
+);
